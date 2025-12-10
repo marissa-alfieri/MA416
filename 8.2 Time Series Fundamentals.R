c80e3a8d076b7b1ae0e8441d2ca9998a29dfba4c
Marissa Alfieri <alfierim@bu.edu>
2025-12-09 23:18:25 -0500

    ugh

--- 8.2 Time Series Fundamentals.R
+++ 8.2 Time Series Fundamentals.R
@@ -1,2 +1,8 @@
 # M08.2 - Time Series Fundamentals
 
+mydata = read.csv("StockMarketB_9.25.csv")
+
+plot(as.POSIXct(mydata$Time), mydata$Stock.Price, pch=16, col='blue')
+lines(as.POSIXct(mydata$Time), mydata$Stock.Price, pch=19, col='black')
+
+
